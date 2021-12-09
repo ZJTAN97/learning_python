eeb33a456cc5698e1a971821b2b3610ff800af8f
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2021-12-09 11:17:11 +0800

    added BFS and halfwaythru dijkstras algorithm

---
+++ Graphs/DijkstrasAlgorithm.py
@@ -0,0 +1,17 @@
+# the graph
+graph = {}
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+
+
+graph["a"] = {}
+graph["a"]["fin"] = 1
+
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+
+graph["fin"] = {}
+
+print(graph)
